1ba0f9d3f6b96915be19a220e120213867084d44
Ajisaputrars <Ajisaputrars@hotmail.com>
2018-11-15 21:06:41 +0700

    Trying pod

--- Concentration/ViewController.swift
+++ Concentration/ViewController.swift
@@ -14,12 +14,11 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+}
 
+extension ViewController{
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
-
