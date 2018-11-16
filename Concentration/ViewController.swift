20f5ea70af7a97c2d7b0ad117839d2c6ac9c2cde
Ajisaputrars <Ajisaputrars@hotmail.com>
2018-11-16 20:50:58 +0700

    Implementing first experiment

--- Concentration/ViewController.swift
+++ Concentration/ViewController.swift
@@ -9,13 +9,32 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var flipCount = 0
+    var emojis = ["👻", "🎃", "👻", "🎃"]
+    @IBOutlet var cardButtons: [UIButton]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
     @IBAction func touchCard(_ sender: UIButton) {
-        
+        flipCount += 1
+        if let cardNumber = cardButtons.index(of: sender) {
+            flipCard(withEmoji: emojis[cardNumber], on: sender)
+        } else {
+            print("Error: Not registered Emoji")
+        }
+    }
+    
+    func flipCard(withEmoji emoji: String, on button: UIButton){
+        if button.currentTitle == emoji {
+            button.setTitle("", for: .normal)
+            button.backgroundColor = UIColor.orange
+        } else {
+            button.setTitle(emojis[cardButtons.index(of: button)!], for: .normal)
+            button.backgroundColor = UIColor.white
+        }
     }
 }
 
