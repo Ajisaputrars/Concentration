b55e02c5fc4a9a29c113eabe6467eb2030e8c114
Ajisaputrars <Ajisaputrars@hotmail.com>
2018-11-19 15:17:39 +0700

    Adding Card struct file

---
+++ Concentration/Card.swift
@@ -0,0 +1,9 @@
+//
+//  Card.swift
+//  Concentration
+//
+//  Created by Aji Saputra Raka Siwi on 19/11/18.
+//  Copyright © 2018 Aji Saputra Raka Siwi. All rights reserved.
+//
+
+import Foundation
